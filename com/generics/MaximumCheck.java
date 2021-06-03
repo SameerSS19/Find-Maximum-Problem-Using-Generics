ad7b1f3bec5a6d2d6932ed58180a83279058e750
SameerSS19 <sk3090019@gmail.com>
2021-06-03 23:54:49 +0530

    [Sameer]Add:Wellcom to Test Maximum problem

---
+++ com/generics/MaximumCheck.java
@@ -0,0 +1,10 @@
+package com.generics;
+
+public class MaximumCheck 
+{
+	 /* Welcome message */
+    public static void main(String[] args) 
+    {
+    	System.out.println("Welcome to the Test Maximum Program");
+	}
+}

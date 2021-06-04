c2d748942eae71091fb5093271d013fb82912a24
SameerSS19 <sk3090019@gmail.com>
2021-06-05 00:05:50 +0530

    [Sameer]Add:Given 3 Float find the maximum

--- com/generics/MaximumCheck.java
+++ com/generics/MaximumCheck.java
@@ -8,13 +8,24 @@ public class MaximumCheck
     	System.out.println("Welcome to the Test Maximum Program");
 	}
 
-    /* maximum of 3 Integer values at test position */
-    public static Integer maximumInteger(Integer firstValue, Integer secondValue, Integer thirdValue) {
+    /*In The Test Position Maximum of 3 Integer values */
+    public static Integer maximumInteger(Integer firstValue, Integer secondValue, Integer thirdValue) 
+    {
         Integer maximumValue = firstValue;
         if (secondValue.compareTo(maximumValue) > 0)
             maximumValue = secondValue;
         if (thirdValue.compareTo(maximumValue) > 0)
             maximumValue = thirdValue;
+        return maximumValue;
+    }
+    /* In The Test Position Maximum of 3 Float values */
+    public static Float maximumFloat(Float first, Float second, Float third) 
+    {
+        Float maximumValue = first; // first is larger
+        if (second.compareTo(maximumValue) > 0)
+            maximumValue = second; // second is larger
+        if (third.compareTo(maximumValue) > 0)
+            maximumValue = third; // third is larger
         return maximumValue;
     }
 }
--- com/generics/MaximumCheckTest.java
+++ com/generics/MaximumCheckTest.java
@@ -5,24 +5,45 @@ import org.junit.Test;
 
 public class MaximumCheckTest {
 
-	/* Maximum Integer element at the position 1 */
+	/* At The Position 1 Maximum Integer element  */
     @Test
     public void maximumIntegerAtPosition1() {
         Integer maximumInteger = MaximumCheck.maximumInteger(22, 7, 14);
         Assert.assertSame(22, maximumInteger);
     }
 
-    /* Maximum Integer element at the position 2 */
+    /* At The Position 2 Maximum Integer element */
     @Test
     public void maximumIntegerAtPosition2() {
         Integer maximumInteger = MaximumCheck.maximumInteger(14, 22, 7);
         Assert.assertSame(22, maximumInteger);
     }
 
-    /* Maximum Integer element at the position 3 */
+    /* At The Position 3 Maximum Integer element */
     @Test
     public void maximumIntegerAtPosition3() {
         Integer maximumInteger = MaximumCheck.maximumInteger(7, 14, 22);
         Assert.assertSame(22, maximumInteger);
+    }
+    
+    /* At The Position 1 Float Integer element */
+    @Test
+    public void maximumFloatAtPosition1() {
+        Float maximumFloat = MaximumCheck.maximumFloat(11.15f, 7.349f, 25.4495f);
+        Assert.assertEquals((Float) 25.4495f, maximumFloat);
+    }
+
+    /* At The Position 2 Float Integer element*/
+    @Test
+    public void maximumFloatAtPosition2() {
+        Float maximumFloat = MaximumCheck.maximumFloat(21.75f, 32.89f, 7.82f);
+        Assert.assertEquals((Float) 32.89f, maximumFloat);
+    }
+
+    /* At The Position 3 Float Integer element */
+    @Test
+    public void maximumFloatAtPosition3() {
+        Float maximumFloat = MaximumCheck.maximumFloat(9.46f, 8.0f, 25.25f);
+        Assert.assertEquals((Float) 25.25f, maximumFloat);
     }
 }
